149393d734e64b2340b79114fc4cd175f4c8a62e
Philippe Kirsch <philippe.kirsch@hotmail.fr>
2017-12-18 17:08:27 +0100

    un peu mieux

--- model.py
+++ model.py
@@ -24,31 +24,30 @@ class BowModel(nn.Module):
 
 
 
-# class BowModel(nn.Module):
-# 	def __init__(self, emb_tensor):
-# 		super(BowModel, self).__init__()
+class BowModel(nn.Module):
+	def __init__(self, emb_tensor):
+		super(BowModel, self).__init__()
 		
-# 		n_embedding, dim = emb_tensor.size()
-# 		self.embedding = nn.Embedding(n_embedding, dim, padding_idx=0)
-# 		self.embedding.weight = Parameter(emb_tensor, requires_grad=False)
+		n_embedding, dim = emb_tensor.size()
+		self.embedding = nn.Embedding(n_embedding, dim, padding_idx=0)
+		self.embedding.weight = Parameter(emb_tensor, requires_grad=False)
 		
-# 		self.hidden_dim = 6
-# 		self.lstm = nn.LSTM(dim, self.hidden_dim)
+		self.hidden_dim = 6
+		self.lstm = nn.LSTM(dim, self.hidden_dim)
 
-# 		self.hidden2sentiment = nn.Linear(dim, 2)
-# 		self.hidden = self.init_hidden()
+		self.hidden2sentiment = nn.Linear(self.hidden_dim, 2)
+		self.hidden = self.init_hidden()
 
-# 	def init_hidden(self):
-# 		return (autograd.Variable(torch.zeros(1, 1, self.hidden_dim)),
-# 				autograd.Variable(torch.zeros(1, 1, self.hidden_dim)))
+	def init_hidden(self):
+		return (autograd.Variable(torch.zeros(1, 1, self.hidden_dim)),
+				autograd.Variable(torch.zeros(1, 1, self.hidden_dim)))
 
-# 	def forward(self, input):
-# 		print(input)
-# 		embeds = self.embedding(input)
-# 		print(embeds.shape)
-# 		lstm_out, self.hidden = self.lstm(embeds.view(len(input), 1, -1), self.hidden)
-		
-# 		tag_space = self.hidden2sentiment(lstm_out.view(len(input), -1))
-# 		tag_scores = F.log_softmax(tag_space)
+	def forward(self, input):
+		self.hidden = self.init_hidden()
+		embeds = self.embedding(input)
+		emb = embeds.transpose(0, 1)
+		lstm_out, self.hidden = self.lstm(emb, self.hidden)
+		tag_space = self.hidden2sentiment(lstm_out[-1])
+		tag_scores = F.log_softmax(tag_space)
 
-# 		return tag_scores
+		return tag_scores
